3d5c8faba4434c4a4864ec774d87f05796dc9c2f
Nathan Cuevas <nathanacuevas97@gmail.com>
2025-01-26 08:57:31 -0800

    fixed auth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,6 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import { authOptions } from './optioins';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // Do whatever you want here, before the request is passed down to `NextAuth`
-  return await NextAuth(req, res, authOptions);
-}
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
